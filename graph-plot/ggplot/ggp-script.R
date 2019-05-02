7a947b22aacc90c5f41d3cc6d2cddbf53f3c61c8
Md Ashaduzzaman <md.ashaduzzaman@selise.ch>
2019-05-02 20:13:01 +0600

    add ggplot param

--- graph-plot/ggplot/ggp-script.R
+++ graph-plot/ggplot/ggp-script.R
@@ -1,3 +1,8 @@
 install.packages("tidyverse")
 library(tidyverse)
-ggplot(data = mpg) + geom_point(mapping = aes(x = displ, y = hwy))
+ggplot(data = mpg) + geom_point(mapping = aes(x = displ, y = hwy, color = class))
+#ggplot(data = mpg) + geom_point(mapping = aes(x = displ, y = hwy, size = class))
+#ggplot(data = mpg) + geom_point(mapping = aes(x = displ, y = hwy, alpha = class))
+
+#make the geom_point function reusable
+#ggplot(data = <DATA>) + <GEOM_FUNCTION>(mapping = aes(<MAPPINGS>))
